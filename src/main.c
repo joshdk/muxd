1121cb0606444c6fbe92fc339ae61d6217363ea4
Josh Komoroske <jdkomo.dev@gmail.com>
2011-11-15 21:31:28 -0500

    [~] -Removed lots of old code.     -Some new comments.

--- src/main.c
+++ src/main.c
@@ -22,15 +22,15 @@ char *strdup(const char *s);
 int main(int argc,char **argv){
 
 	openlog("muxd",LOG_NDELAY|LOG_PID,LOG_DAEMON);
-	syslog(LOG_NOTICE,"starting muxd");
-
+	syslog(LOG_NOTICE,"#################");
+	syslog(LOG_NOTICE,"# starting muxd #");
+	syslog(LOG_NOTICE,"#################");
 
 	if(argc<3){
 		syslog(LOG_ERR,"insufficient parameters");
 		syslog(LOG_CRIT,"stopping muxd");
 		return 1;
 	}
-
 
 	syslog(LOG_INFO,"opening config file %s",argv[1]);
 
@@ -57,10 +57,6 @@ int main(int argc,char **argv){
 		return 1;
 	}
 
-
-	//printf("local:  [%s:%s]\n",laddr,lport);
-
-
 	service **services=NULL;
 	if(config_get_services(&conf,&services)){
 		syslog(LOG_ERR,"failed to read services");
@@ -69,6 +65,8 @@ int main(int argc,char **argv){
 	}
 
 	config_close(&conf);
+
+
 
 
 	syslog(LOG_INFO,"opening magic file %s",argv[2]);
@@ -90,24 +88,16 @@ int main(int argc,char **argv){
 
 
 
-	int fdlsocket=-1;
-	int fdrsocket=-1;
-
-
-
 
 	struct addrinfo hints, *servinfo;
-
-
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags=2;//AI_CANONNAME;
-
-	//local host lookup
+	hints.ai_flags=AI_PASSIVE;
 	
 	syslog(LOG_INFO,"looking up local address [%s]:%s",laddr,lport);
 
+	//local host lookup
 	if((getaddrinfo(laddr,lport,&hints,&servinfo))!=0){
 		syslog(LOG_ERR,"failed to lookup local address [%s]:%s",laddr,lport);
 		syslog(LOG_CRIT,"stopping muxd");
@@ -115,29 +105,22 @@ int main(int argc,char **argv){
 	}
 
 	int lsocket=-1;
+	int fdlsocket=-1;
 	int bind_success=0;
 	for(struct addrinfo *p=servinfo;p!=NULL;p=p->ai_next){
-
 		//create local socket
 		if((lsocket=socket(p->ai_family,p->ai_socktype,p->ai_protocol))<0){
-			//perror("[-] socket");
 			continue;
-		}else{
-			//puts("[+] socket");
 		}
 
 		//set socket option
 		int flags=1;
 		if(setsockopt(lsocket,SOL_SOCKET,SO_REUSEADDR,&flags,sizeof(flags))!=0){
-			//perror("[-] setsockopt");
 			return 1;
-		}else{
-			//puts("[+] setsockopt");
 		}
 
 		//bind to local address
 		if(bind(lsocket,p->ai_addr,p->ai_addrlen)<0){
-			//perror("[-] bind");
 			continue;
 		}else{
 			syslog(LOG_INFO,"bound to local address [%s]:%s",laddr,lport);
@@ -158,39 +141,39 @@ int main(int argc,char **argv){
 	free(laddr);
 	free(lport);
 
-	//drop privs
+
+
+
 	gid_t gid=500;
 	uid_t uid=500;
-	syslog(LOG_INFO,"droping privliages to %d:%d",uid,gid);
+	syslog(LOG_INFO,"droping privileges to %d:%d",uid,gid);
 
+	//drop user privileges
 	if(setgid(gid)!=0){
-		syslog(LOG_ERR,"failed to drop privliages to group:%d",gid);
+		syslog(LOG_ERR,"failed to drop privileges to group:%d",gid);
 		syslog(LOG_CRIT,"stopping muxd");
 		return 1;
 	}
-	//puts("[+] setgid");
 
-	//drop privs
-	//syslog(LOG_INFO,"attempting to drop privliages to user:%d",uid);
+	//drop group privileges
 	if(setuid(uid)!=0){
-		syslog(LOG_ERR,"failed to drop privliages to user:%d",uid);
+		syslog(LOG_ERR,"failed to drop privileges to user:%d",uid);
 		syslog(LOG_CRIT,"stopping muxd");
 		return 1;
 	}
-	//puts("[+] setuid");
 
 
+
+
+	syslog(LOG_INFO,"listening on socket");
 
 	//listen on port
-	syslog(LOG_INFO,"listening on socket");
-	if(listen(lsocket,3)!=0){
+	int backlog=16;
+	if(listen(lsocket,backlog)!=0){
 		syslog(LOG_ERR,"failed to listen on socket");
 		syslog(LOG_CRIT,"stopping muxd");
 		return 1;
 	}
-
-
-
 
 	struct sockaddr_storage sas;
 	socklen_t addrlen=sizeof(sas);
@@ -199,28 +182,21 @@ int main(int argc,char **argv){
 
 	//accept client connections
 	while((fdlsocket=accept(lsocket,(struct sockaddr *)&sas,&addrlen))>=0){
-		//puts("[+] accept");
 
 		const struct sockaddr *client=(struct sockaddr *)&sas;
 		if(getnameinfo(client,addrlen,bhost,INET6_ADDRSTRLEN+1,bport,16+1,NI_NUMERICHOST|NI_NUMERICSERV)==0){
-			//printf("name success\n");
-			//printf("host: [%s]\n",bhost);
-			//printf("port: [%s]\n",bport);
 			syslog(LOG_INFO,"accepted client [%s]:%s",bhost,bport);
 		}else{
-			//printf("name failure\n");
 			syslog(LOG_INFO,"accepted client [???]:???");
 		}
 
-		//break;
 		pid_t child=0;
 		if((child=fork())==0){
-			//puts("[+] child fork");
 			break;
 		}else{
-			//printf("[+] parent fork (%d)\n",child);
 			syslog(LOG_INFO,"created worker %d",child);
 		}
+
 	}
 
 	free(bhost);
@@ -228,9 +204,7 @@ int main(int argc,char **argv){
 
 
 
-	//puts("[-] accept");
-	//return 1;
-
+	//read handshake from client
 	int lbuffer=0;
 	int sbuffer=1024;
 	char *buffer=NULL;
@@ -244,7 +218,6 @@ int main(int argc,char **argv){
 		free(buffer);
 		return 1;
 	}
-	//printf("buffer: [%s]\n",buffer);
 
 
 	service *service=NULL;
@@ -257,8 +230,10 @@ int main(int argc,char **argv){
 		free(buffer);
 		return 1;
 	}
-	//printf("[+] magic_get_service\n");
+
 	magic_close(cookie);
+
+
 
 
 	char *raddr=NULL;
@@ -266,13 +241,6 @@ int main(int argc,char **argv){
 	raddr=strdup(service->host);
 	rport=strdup(service->port);
 	free_services(services);
-	//printf("remote: [%s:%s]\n",raddr,rport);
-
-	//free(buffer);
-	//shutdown(fdlsocket,SHUT_RDWR);
-	//shutdown(fdrsocket,SHUT_RDWR);
-	//return 0;
-
 
 	syslog(LOG_INFO,"looking up remote address [%s]:%s",raddr,rport);
 
@@ -285,26 +253,19 @@ int main(int argc,char **argv){
 		free(rport);
 		return 1;
 	}
-	//puts("[+] getaddrinfo");
 
+	int fdrsocket=-1;
 	int connect_success=0;
 	for(struct addrinfo *p=servinfo;p!=NULL;p=p->ai_next){
-
 		//create remote socket
 		if((fdrsocket=socket(p->ai_family,p->ai_socktype,p->ai_protocol))<0){
-			//perror("[-] socket");
 			continue;
-		}else{
-			//puts("[+] socket");
 		}
 
 		//connect to remore host
 		if(connect(fdrsocket,p->ai_addr,p->ai_addrlen)==-1) {
-			//close(sockfd);
-			//puts("[-] connect");
 			continue;
 		}else{
-			//puts("[+] connect");
 			syslog(LOG_INFO,"connected to remote address [%s]:%s",raddr,rport);
 			connect_success=1;
 			break;
@@ -313,7 +274,6 @@ int main(int argc,char **argv){
 	}
 
 	freeaddrinfo(servinfo);
-
 
 	if(connect_success==0){
 		syslog(LOG_ERR,"failed to connect to remote address [%s]:%s",raddr,rport);
@@ -326,7 +286,11 @@ int main(int argc,char **argv){
 
 	free(raddr);
 	free(rport);
-	
+
+
+
+
+	//send handshake to remote host
 	int bs=-1;
 	if((bs=send(fdrsocket,buffer,lbuffer,0))<=0){
 		syslog(LOG_ERR,"failed to send");
@@ -340,20 +304,10 @@ int main(int argc,char **argv){
 
 
 
-	//shutdown(fdlsocket,SHUT_RDWR);
-	//shutdown(fdrsocket,SHUT_RDWR);
-	//free_services(services);
-	//return 0;
-
-
 
 	pid_t worker=fork();
 	if(worker>0){
 		syslog(LOG_DEBUG,"local shuttle started");
-		//shutdown(fdlsocket,SHUT_WR);//we won't write to local
-		//shutdown(fdrsocket,SHUT_RD);//we won't read from remote
-		//puts("[+] fork child");
-		//puts("[!] server start");
 		int size=4096;
 		char *shuttle=malloc(size*sizeof(char));
 		while(1){
@@ -376,27 +330,13 @@ int main(int argc,char **argv){
 		}
 
 		free(shuttle);
-		//shuttle=NULL;
-		//puts("[!] server done");
-		//puts("[!] remote shutdown");
 		syslog(LOG_DEBUG,"local shuttle finished");
 		shutdown(fdlsocket,SHUT_RDWR);
 		shutdown(fdrsocket,SHUT_RDWR);
-		//if(shutdown(fdrsocket,2)!=0){
-			//perror("[-] shutdown");
-		//}else{
-			//perror("[+] shutdown");
-		//}
-		//printf("waiting on child: %d\n",worker);
 		waitpid(worker,NULL,0);
-		//printf("done waiting on child: %d\n",worker);
 
 	}else{
 		syslog(LOG_DEBUG,"remote shuttle started");
-		//shutdown(fdlsocket,SHUT_RD);//we won't read from local
-		//shutdown(fdrsocket,SHUT_WR);//we won't write to remote
-		//puts("[+] fork parent");
-		//puts("[!] client start");
 		int size=4096;
 		char *shuttle=malloc(size*sizeof(char));
 		while(1){
@@ -419,23 +359,17 @@ int main(int argc,char **argv){
 		}
 
 		free(shuttle);
-		//shuttle=NULL;
-		//puts("[!] client done");
-		//puts("[!] local shutdown");
 		syslog(LOG_DEBUG,"remote shuttle finished");
 		shutdown(fdlsocket,SHUT_RDWR);
 		shutdown(fdrsocket,SHUT_RDWR);
-		//if(shutdown(fdrsocket,2)!=0){
-			//perror("[-] shutdown");
-		//}else{
-			//perror("[+] shutdown");
-		//}
-		//printf("child exiting\n");
-		exit(0);
+		return 0;
 	}
 
+
+
+
+	//all done with this worker
 	syslog(LOG_INFO,"worker finished normally");
 	syslog(LOG_INFO,"stopping muxd worker");
-	exit(0);
 	return 0;
 }
